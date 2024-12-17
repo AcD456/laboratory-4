f8f40917bb7887c493c24dbd17546d40087c0dc3
Dmitri <dmitri.lifantev@gmail.com>
2024-12-17 16:29:14 +0300

    first commit

---
+++ run.py
@@ -0,0 +1,31 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+from config import TOKEN
+
+import logging
+
+bot = Bot(token = TOKEN)
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer('Привет')
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    logging.basicConfig(level = logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
+
+
+
